d0e0e285dabb00746d0257ce4ef869b917e01e7b
Ryan Hileman <lunixbochs@gmail.com>
2025-04-15 01:04:38 -0700

    fix building on mingw

--- skia-bindings/src/gl.cpp
+++ skia-bindings/src/gl.cpp
@@ -150,12 +150,12 @@ extern "C" const GrGLInterface* C_GrGLInterface_MakeAssembledInterface(void *ctx
 // gpu/GrDirectContext.h
 //
 
-extern "C" GrDirectContext* C_GrDirectContext_MakeGL(GrGLInterface* interface, const GrContextOptions* options) {
-    if (interface) {
+extern "C" GrDirectContext* C_GrDirectContext_MakeGL(GrGLInterface* iface, const GrContextOptions* options) {
+    if (iface) {
         if (options) {
-            return GrDirectContexts::MakeGL(sp(interface), *options).release();
+            return GrDirectContexts::MakeGL(sp(iface), *options).release();
         }
-        return GrDirectContexts::MakeGL(sp(interface)).release();
+        return GrDirectContexts::MakeGL(sp(iface)).release();
     }
     if (options) {
         return GrDirectContexts::MakeGL(*options).release();
